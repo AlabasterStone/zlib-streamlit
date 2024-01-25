6c8dd9ba59b0bcab17c1bc989a0bb1c9c3ce17dc
tourkveg <thewavylines@outlook.com>
2024-01-25 10:17:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,15 +16,14 @@ if st.button('搜索'):
     book_list.append(f'{book["title"]}|{book["extension"]}|{book["language"]}')
   choice = st.selectbox('选择下载',book_list)
   st.text(choice)
-  if st.button("确认"):
-      st.text(choice)
-      filename, filecontent = z.downloadBook(res['books'][book_list.index(choice)])
-      file_name = f"book.{filename.split('.')[1]}"
-      with open(file_name, "wb") as bookfile:
-        bookfile.write(filecontent)
-      with open(file_name, "rb") as file:
-        btn = st.download_button(
-                  label="下载文件",
-                  data=file,
-                  file_name=file_name
-                )
+  st.text(choice)
+  filename, filecontent = z.downloadBook(res['books'][book_list.index(choice)])
+  file_name = f"book.{filename.split('.')[1]}"
+  with open(file_name, "wb") as bookfile:
+    bookfile.write(filecontent)
+  with open(file_name, "rb") as file:
+    btn = st.download_button(
+              label="下载文件",
+              data=file,
+              file_name=file_name
+            )
