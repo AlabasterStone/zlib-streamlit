68a4b66480bf0562a8d7fcca3c97a8b7d5487bdf
tourkveg <thewavylines@outlook.com>
2024-01-25 10:27:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,25 @@ book_name = st.text_input('书名')
 number = st.slider('数量限制',1,100)
 
 book_list = list()
-search_button = st.button('搜索')
-
+def search():
+  z = Zlibrary()
+  z.login(email=email, password=password)
+  res = z.search(message=book_name,limit=number)
+  for book in res['books']:
+    book_list.append(f'{book["title"]}|{book["extension"]}|{book["language"]}')
+  choice = st.selectbox('选择下载',book_list)
+  filename, filecontent = z.downloadBook(res['books'][book_list.index(choice)])
+  file_name = f"book.{filename.split('.')[1]}"
+  with open(file_name, "wb") as bookfile:
+    bookfile.write(filecontent)
+  with open(file_name, "rb") as file:
+    btn = st.download_button(
+              label="下载文件",
+              data=file,
+              file_name=file_name
+            )
+st.button('搜索', on_click=search)
+'''
 if search_button:
   z = Zlibrary()
   z.login(email=email, password=password)
@@ -27,3 +44,4 @@ if search_button:
               data=file,
               file_name=file_name
             )
+'''
