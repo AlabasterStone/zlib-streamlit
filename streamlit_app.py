b48c476e30fc86d9c3f05c3eab65decbc316c9c5
tourkveg <thewavylines@outlook.com>
2024-01-25 09:31:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 from zlibrary import Zlibrary
-z = Zlibrary()
-z.login(email="mulanhuayun@163.com", password="7811204630zhym")
-res = z.search(message="儒林外史",limit=10)
-for book in res['books']:
-  print(f'{book["title"]}|{book["extension"]}|{book["language"]}')
+import streamlit as st
+
+st.title('Zlibrary API hosted by tourkveg')
+email = st.text_input('email')
+password = st.text_input('password', type='password')
+book_name = st.text_input('书名')
+number = st.slider('数量限制',1,100)
+
+book_list = list()
+if st.button('搜索'):
+  z = Zlibrary()
+  z.login(email=email, password=password)
+  res = z.search(message=book_name,limit=number)
+  for book in res['books']:
+    book_list.append(f'{book["title"]}|{book["extension"]}|{book["language"]}')
+  choice = st.selectbox('选择下载',book_list)
+  # Downloading a book
+  filename, filecontent = z.downloadBook(res['books'][book_list.index(choice)])
+  st.download_button("下载文件", filecontent)
